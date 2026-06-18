9a18003dd4ef6e245afe1e2b0c57a367b06705e2
SagarNeupane <sagarneupane@policybazaar.com>
2026-06-18 17:23:53 +0530

    fix

--- app/main.py
+++ app/main.py
@@ -1,10 +1,6 @@
 from fastapi import FastAPI
 
-from app.routes import router
-
 app = FastAPI(title="FastAPI App", version="0.1.0")
-
-app.include_router(router, prefix="/api")
 
 
 @app.get("/health")
