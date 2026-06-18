df0a110b00c0658b3ca6432e3442435cdce776ef
SagarNeupane <sagarneupane@policybazaar.com>
2026-06-18 17:39:54 +0530

    api prefix

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 
+from app.routes.customer_routes import router as customer_router
+
 app = FastAPI(title="FastAPI App", version="0.1.0")
+
+app.include_router(customer_router, prefix="/api")
 
 
 @app.get("/health")
